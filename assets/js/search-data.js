9958a3f4530f93964da265b601eb22d699af15eb
AymanAbaid <aymanabaid7@gmail.com>
2025-08-11 01:14:41 +0000

    Deploying to gh-pages from @ AymanAbaid/aymanabaid.github.io@4c104cb514c6a7e257b9f498bf54950c239cd9b2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -471,14 +471,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61.%61%62%61%69%64%31@%75%6E%69%76%65%72%73%69%74%79%6F%66%67%61%6C%77%61%79.%69%65", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-leetcode',
+        title: 'LeetCode',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://leetcode.com/u//", "_blank");
         },
       },{
         id: 'social-rss',
@@ -492,14 +492,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=byNZTQ8AAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
