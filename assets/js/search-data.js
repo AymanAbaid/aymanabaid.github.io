2b27f44f01e7d036ddb62cdac5e5a193b248632a
AymanAbaid <aymanabaid7@gmail.com>
2025-08-11 01:37:32 +0000

    Deploying to gh-pages from @ AymanAbaid/aymanabaid.github.io@bb99eebd5eea101d3af546b4e2b841b99f03470d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,12 +16,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-talks-and-posters",
-          title: "Talks and Posters",
+        },{id: "nav-talks",
+          title: "Talks",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/presentation/";
+            window.location.href = "/teaching/";
           },
         },{id: "nav-cv",
           title: "CV",
